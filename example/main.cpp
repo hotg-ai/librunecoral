2ecd7e9f99398f559b5501e5d0430c659203ead3
Dinesh Manajipet <saidinesh5@gmail.com>
2021-08-11 12:12:51 +0530

    example: Fix the example to work with the new API

--- example/main.cpp
+++ example/main.cpp
@@ -9,12 +9,12 @@ extern "C" {
 int main(int argc, char* argv[]) {
     std::vector<float> inputData = {0.8};
     std::vector<float> outputData = {0};
-    std::vector<int> tensorShape = {1,1};
+    std::vector<size_t> tensorShape = {1,1};
 
     RuneCoralContext  *context = nullptr;
 
-    RuneCoralTensor inputs[] = { {RuneCoralElementType__Float32, inputData.data(), tensorShape.data(), static_cast<int>(tensorShape.size())} };
-    RuneCoralTensor outputs[] = { {RuneCoralElementType__Float32, outputData.data(), tensorShape.data(), static_cast<int>(tensorShape.size())} };
+    RuneCoralTensor inputs[] = { {RuneCoralElementType__Float32, inputData.data(), tensorShape.data(), tensorShape.size()} };
+    RuneCoralTensor outputs[] = { {RuneCoralElementType__Float32, outputData.data() , tensorShape.data(), tensorShape.size()} };
 
     auto contextCreationResult = create_inference_context(RUNE_CORAL_MIME_TYPE__TFLITE,
                                                           reinterpret_cast<const void*>(Resources::sinemodel_tflite), Resources::sinemodel_tflite_size,
@@ -33,6 +33,6 @@ int main(int argc, char* argv[]) {
         std::cerr << "Error Creating context: " << static_cast<int>(contextCreationResult) << std::endl;
     }
 
-    destroy_inference_context(&context);
+    destroy_inference_context(context);
     return 0;
 }
